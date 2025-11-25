592fd2e6855fe546ed533f392da99fc758612d0e
AlphaTech Apps <alphatech.apps.bd@gmail.com>
2025-11-26 00:29:25 +0600

    2.0 no activity only Library

--- pref/src/main/java/com/jakir/pref/Pref.java
+++ pref/src/main/java/com/jakir/pref/Pref.java
@@ -30,8 +30,6 @@ public class Pref {
         SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
         return  preferences.getInt(key,1);
     }
-
-
     public static void setfValue_default1(float data, String key, Context context){
         SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
         editor.putFloat(key,data);
@@ -41,7 +39,6 @@ public class Pref {
         SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
         return  preferences.getFloat(key,1);
     }
-
     public static void setiValue(int data, String key, Context context){
         SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
         editor.putInt(key,data);
@@ -51,7 +48,6 @@ public class Pref {
         SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
         return  preferences.getInt(key,0);
     }
-
     public static void setAmount(int data, Context context){
         SharedPreferences.Editor editor = context.getSharedPreferences("amount",Context.MODE_PRIVATE).edit();
         editor.putInt("amount",data);
@@ -61,15 +57,10 @@ public class Pref {
         SharedPreferences preferences = context.getSharedPreferences("amount",Context.MODE_PRIVATE);
         return  preferences.getInt("amount",0);
     }
-
     public static void setState(boolean data, String key, Context context){
         SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
         editor.putBoolean(key,data);
         editor.apply();
-    }
-    public static boolean getState(String key,Context context){
-        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
-        return  preferences.getBoolean(key,false);
     }
     public static void setStatep(boolean data, String key, Context context){
         SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
@@ -86,10 +77,85 @@ public class Pref {
         editor.putInt("ThemeMode", data);
         editor.apply();
     }
-
     public static int getTheme(Context context) {
         SharedPreferences preferences = context.getSharedPreferences("ThemePrefs", Context.MODE_PRIVATE);
         return preferences.getInt("ThemeMode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
     }
 
+
+
+
+    public static void setString(String key,String data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putString(key,data);
+        editor.apply();
+    }
+    public static String getString(String key,Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getString(key,"");
+    }
+    public static void setInt( String key, int data,Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putInt(key,data);
+        editor.apply();
+    }
+    public static int getInt(String key, Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getInt(key,0);
+    }
+    public static void setInt_default1( String key,int data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putInt(key,data);
+        editor.apply();
+    }
+    public static int getInt_default1(String key, Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getInt(key,1);
+    }
+    public static void setInt_default_1(String key, int data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putInt(key,data);
+        editor.apply();
+    }
+    public static int getInt_default_1(String key, Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getInt(key,-1);
+    }
+    public static void setFloat( String key,float data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putFloat(key,data);
+        editor.apply();
+    }
+    public static float getFloat(String key, Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getFloat(key,0);
+    }
+    public static void setFloat_default1( String key,float data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putFloat(key,data);
+        editor.apply();
+    }
+    public static float getFloat_default1(String key, Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getFloat(key,1);
+    }
+    public static void setState( String key,boolean data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putBoolean(key,data);
+        editor.apply();
+    }
+    public static boolean getState(String key,Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getBoolean(key,false);
+    }
+    public static void setState_defaultP( String key,boolean data, Context context){
+        SharedPreferences.Editor editor = context.getSharedPreferences(key,Context.MODE_PRIVATE).edit();
+        editor.putBoolean(key,data);
+        editor.apply();
+    }
+    public static boolean getState_defaultP(String key,Context context){
+        SharedPreferences preferences = context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        return  preferences.getBoolean(key,true);
+
+    }
 }
